310c132854ba13ea97080f045b7883a014456313
Yariv <yariv.gdidi@gmail.com>
2021-08-17 19:44:04 +0300

    mongoose query - download file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment')
+const uuid = require('uuid');
+const fs = require('fs');
 const { db } = require('./db')
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const { CustomerInvoice } = require('./db/model')
 
 const app = express();
@@ -20,12 +23,37 @@ app.post('/search', async (req, res) => {
                         $lt: end
                     }
         })
-        res.send({data: invoices})
+        const fileId = uuid.v4()
+        const csvres = await writeCsv(invoices, fileId)
+        res.send({data: `http://localhost:3000/downlaod/${fileId}`})
+
     } catch (err) {
-        res.status(500).send('Server Error');
+        res.status(500).send(`Server Error: ${err.message}`);
+    }
+})
+app.get('/downlaod/:fileId', async (req, res) => {
+    try {
+        const { fileId } = req.params;
+        const file = `public/results-${fileId}.csv`;
+        res.download(file)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(`Server Error: ${err.message}`);
     }
 })
 
+const writeCsv = async (data, uuid) => {
+    const csvWriter = createCsvWriter({
+        path: `public/results-${uuid}.csv`,
+        header: [
+            {id: 'customerId', title: 'Customer Id'},
+            {id: 'invoiceId', title: 'Invoice Id'},
+            {id: 'created_at', title: 'Created At'},
+        ]
+    });
+    return await csvWriter.writeRecords(data)
+
+}
 app.listen(3000, ()=> {
     console.log('Listening on port 3000' );
 })
